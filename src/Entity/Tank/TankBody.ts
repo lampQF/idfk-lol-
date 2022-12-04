b16b67ceea2a28d155d102e4fcaae875b86c972d
Sodaposa <71281147+soda-tm@users.noreply.github.com>
2022-12-04 02:13:15 -0500

    Update TankBody.ts

--- src/Entity/Tank/TankBody.ts
+++ src/Entity/Tank/TankBody.ts
@@ -292,7 +292,7 @@ export default class TankBody extends LivingEntity implements BarrelBase {
             this.styleData.opacity -= this.definition.invisibilityRate;
 
             this.styleData.opacity = util.constrain(this.styleData.values.opacity, 0, 1);
-            console.log(this.cameraEntity.cameraData.values.statLevels.values[Stat.MaxHealth])
+            this.damageReduction = ((1 - this.styleData.opacity) * 0.5) - this.cameraEntity.cameraData.values.statLevels.values[Stat.MaxHealth] * 0.05
         }
 
 
