25435dd0b45f299d12b6cfec3049cb1f70217cb6
Sodaposa <71281147+soda-tm@users.noreply.github.com>
2022-12-03 21:21:39 -0500

    Update TankBody.ts

--- src/Entity/Tank/TankBody.ts
+++ src/Entity/Tank/TankBody.ts
@@ -1,14 +1,17 @@
 /*
     DiepCustom - custom tank game server that shares diep.io's WebSocket protocol
     Copyright (C) 2022 ABCxFF (github.com/ABCxFF)
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
     by the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
+
     You should have received a copy of the GNU Affero General Public License
     along with this program. If not, see <https://www.gnu.org/licenses/>
 */
@@ -288,7 +291,7 @@ export default class TankBody extends LivingEntity implements BarrelBase {
            
             this.styleData.opacity -= this.definition.invisibilityRate;
 
-            this.styleData.opacity = util.constrain(this.styleData.values.opacity, 0, 1)
+            this.styleData.opacity = util.constrain(this.styleData.values.opacity, 0, 1);
         }
 
 
@@ -312,9 +315,6 @@ export default class TankBody extends LivingEntity implements BarrelBase {
 
             // Reload
             this.reloadTime = 15 * Math.pow(0.914, this.cameraEntity.cameraData.values.statLevels.values[Stat.Reload]);
-            
-            // Opacity Defense
-            this.damageReduction = 0.5 * this.styleData.opacity;
         }
 
         this.scoreData.score = this.cameraEntity.cameraData.values.score;
